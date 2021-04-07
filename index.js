cdcc07fb84e5b84684af7fcddb78ddc8740aa025
007 <65741741+6gx7iycn53ioq2e8apk1j1ypwov4giui@users.noreply.github.com>
2021-04-07 20:31:29 +0300

    add experimental modules
    * Update index.js
    
    * remove duplication
    
    * Update index.js
    
    * add trace_events
    
    * add inspector
    
    * sorted versions
    
    * add experimetal feature flags
    
    * remove experimental flags
    
    * add { experimental } option
    
    Co-authored-by: Julian Gruber <julian@juliangruber.com>

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 
 var semver = require('semver')
 
-module.exports = function (version) {
-  version = version || process.version
-
+module.exports = function ({
+  version = process.version,
+  experimental = false
+} = {}) {
   var coreModules = [
     'assert',
     'buffer',
@@ -43,9 +44,21 @@ module.exports = function (version) {
   if (semver.lt(version, '6.0.0')) coreModules.push('freelist')
   if (semver.gte(version, '1.0.0')) coreModules.push('v8')
   if (semver.gte(version, '1.1.0')) coreModules.push('process')
+  if (semver.gte(version, '8.0.0')) coreModules.push('inspector')
   if (semver.gte(version, '8.1.0')) coreModules.push('async_hooks')
   if (semver.gte(version, '8.4.0')) coreModules.push('http2')
   if (semver.gte(version, '8.5.0')) coreModules.push('perf_hooks')
+  if (semver.gte(version, '10.0.0')) coreModules.push('trace_events')
 
+  if (
+    semver.gte(version, '10.5.0') &&
+    (experimental || semver.gte(version, '12.0.0'))
+  ) {
+    coreModules.push('worker_threads')
+  }
+  if (semver.gte(version, '12.16.0') && experimental) {
+    coreModules.push('wasi')
+  }
+  
   return coreModules
 }
