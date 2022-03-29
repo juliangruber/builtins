46cc1871dcd6e65014122e8f2a66b8f59e83baa8
Julian Gruber <julian@juliangruber.com>
2022-03-29 11:36:13 +0200

    modernize

--- test.js
+++ test.js
@@ -1,55 +1,55 @@
 'use strict'
 
-var test = require('tape')
-var builtins = require('./')
+const test = require('tape')
+const builtins = require('./')
 
-test('freelist', function (t) {
-  t.ok(builtins({ version: '5.99.99' }).indexOf('freelist') !== -1)
-  t.ok(builtins({ version: '6.0.0' }).indexOf('freelist') === -1)
+test('freelist', t => {
+  t.ok(builtins({ version: '5.99.99' }).includes('freelist'))
+  t.notOk(builtins({ version: '6.0.0' }).includes('freelist'))
   t.end()
 })
 
-test('v8', function (t) {
-  t.ok(builtins({ version: '0.99.99' }).indexOf('v8') === -1)
-  t.ok(builtins({ version: '1.0.0' }).indexOf('v8') !== -1)
+test('v8', t => {
+  t.notOk(builtins({ version: '0.99.99' }).includes('v8'))
+  t.ok(builtins({ version: '1.0.0' }).includes('v8'))
   t.end()
 })
 
-test('process', function (t) {
-  t.ok(builtins({ version: '1.0.99' }).indexOf('process') === -1)
-  t.ok(builtins({ version: '1.1.0' }).indexOf('process') !== -1)
+test('process', t => {
+  t.notOk(builtins({ version: '1.0.99' }).includes('process'))
+  t.ok(builtins({ version: '1.1.0' }).includes('process'))
   t.end()
 })
 
-test('async_hooks', function (t) {
-  t.ok(builtins({ version: '8.0.99' }).indexOf('async_hooks') === -1)
-  t.ok(builtins({ version: '8.1.0' }).indexOf('async_hooks') !== -1)
+test('async_hooks', t => {
+  t.notOk(builtins({ version: '8.0.99' }).includes('async_hooks'))
+  t.ok(builtins({ version: '8.1.0' }).includes('async_hooks'))
   t.end()
 })
 
-test('http2', function (t) {
-  t.ok(builtins({ version: '8.3.99' }).indexOf('http2') === -1)
-  t.ok(builtins({ version: '8.4.0' }).indexOf('http2') !== -1)
+test('http2', t => {
+  t.notOk(builtins({ version: '8.3.99' }).includes('http2'))
+  t.ok(builtins({ version: '8.4.0' }).includes('http2'))
   t.end()
 })
 
-test('perf_hooks', function (t) {
-  t.ok(builtins({ version: '8.4.99' }).indexOf('perf_hooks') === -1)
-  t.ok(builtins({ version: '8.5.0' }).indexOf('perf_hooks') !== -1)
+test('perf_hooks', t => {
+  t.notOk(builtins({ version: '8.4.99' }).includes('perf_hooks'))
+  t.ok(builtins({ version: '8.5.0' }).includes('perf_hooks'))
   t.end()
 })
 
-test('node 0.12.0 count', function (t) {
+test('node 0.12.0 count', t => {
   t.equal(builtins({ version: '0.12.0' }).length, 33)
   t.end()
 })
 
-test('node 8.5.0 count', function (t) {
+test('node 8.5.0 count', t => {
   t.equal(builtins({ version: '8.5.0' }).length, 38)
   t.end()
 })
 
-test('worker_threads', function (t) {
+test('worker_threads', t => {
   t.notOk(builtins({ version: '8.5.0' }).includes('worker_threads'))
   t.notOk(builtins({ version: '10.5.0' }).includes('worker_threads'))
   t.ok(
@@ -61,13 +61,13 @@ test('worker_threads', function (t) {
   t.end()
 })
 
-test('wasi', function (t) {
+test('wasi', t => {
   t.notOk(builtins({ version: '12.16.0' }).includes('wasi'))
   t.ok(builtins({ version: '12.16.0', experimental: true }).includes('wasi'))
   t.end()
 })
 
-test('diagnostics_channel', function (t) {
+test('diagnostics_channel', t => {
   t.notOk(
     builtins({ version: '15.0.0', experimental: true }).includes(
       'diagnostics_channel'
@@ -105,14 +105,14 @@ test('diagnostics_channel', function (t) {
   t.end()
 })
 
-test('default to current version', function (t) {
-  builtins().forEach(function (name) {
+test('default to current version', t => {
+  for (const name of builtins()) {
     t.ok(require(name), name)
-  })
+  }
   t.end()
 })
 
-test('returns all builtins with version *', function (t) {
+test('returns all builtins with version *', t => {
   t.equal(builtins({ version: '*' }).length, 32 + 9)
   t.equal(builtins({ version: '*', experimental: true }).length, 32 + 9 + 2)
   t.end()
