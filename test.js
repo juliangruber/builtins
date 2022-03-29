7c08e2be9103756bdce31df7440b4a032f13b111
Julian Gruber <julian@juliangruber.com>
2022-03-29 11:38:59 +0200

    switch from tape to node-core-test

--- test.js
+++ test.js
@@ -1,119 +1,109 @@
 'use strict'
 
-const test = require('tape')
+const test = require('node-core-test')
+const assert = require('assert')
 const builtins = require('./')
 
 test('freelist', t => {
-  t.ok(builtins({ version: '5.99.99' }).includes('freelist'))
-  t.notOk(builtins({ version: '6.0.0' }).includes('freelist'))
-  t.end()
+  assert(builtins({ version: '5.99.99' }).includes('freelist'))
+  assert(!builtins({ version: '6.0.0' }).includes('freelist'))
 })
 
 test('v8', t => {
-  t.notOk(builtins({ version: '0.99.99' }).includes('v8'))
-  t.ok(builtins({ version: '1.0.0' }).includes('v8'))
-  t.end()
+  assert(!builtins({ version: '0.99.99' }).includes('v8'))
+  assert(builtins({ version: '1.0.0' }).includes('v8'))
 })
 
 test('process', t => {
-  t.notOk(builtins({ version: '1.0.99' }).includes('process'))
-  t.ok(builtins({ version: '1.1.0' }).includes('process'))
-  t.end()
+  assert(!builtins({ version: '1.0.99' }).includes('process'))
+  assert(builtins({ version: '1.1.0' }).includes('process'))
 })
 
 test('async_hooks', t => {
-  t.notOk(builtins({ version: '8.0.99' }).includes('async_hooks'))
-  t.ok(builtins({ version: '8.1.0' }).includes('async_hooks'))
-  t.end()
+  assert(!builtins({ version: '8.0.99' }).includes('async_hooks'))
+  assert(builtins({ version: '8.1.0' }).includes('async_hooks'))
 })
 
 test('http2', t => {
-  t.notOk(builtins({ version: '8.3.99' }).includes('http2'))
-  t.ok(builtins({ version: '8.4.0' }).includes('http2'))
-  t.end()
+  assert(!builtins({ version: '8.3.99' }).includes('http2'))
+  assert(builtins({ version: '8.4.0' }).includes('http2'))
 })
 
 test('perf_hooks', t => {
-  t.notOk(builtins({ version: '8.4.99' }).includes('perf_hooks'))
-  t.ok(builtins({ version: '8.5.0' }).includes('perf_hooks'))
-  t.end()
+  assert(!builtins({ version: '8.4.99' }).includes('perf_hooks'))
+  assert(builtins({ version: '8.5.0' }).includes('perf_hooks'))
 })
 
 test('node 0.12.0 count', t => {
-  t.equal(builtins({ version: '0.12.0' }).length, 33)
-  t.end()
+  assert.strictEqual(builtins({ version: '0.12.0' }).length, 33)
 })
 
 test('node 8.5.0 count', t => {
-  t.equal(builtins({ version: '8.5.0' }).length, 38)
-  t.end()
+  assert.strictEqual(builtins({ version: '8.5.0' }).length, 38)
 })
 
 test('worker_threads', t => {
-  t.notOk(builtins({ version: '8.5.0' }).includes('worker_threads'))
-  t.notOk(builtins({ version: '10.5.0' }).includes('worker_threads'))
-  t.ok(
+  assert(!builtins({ version: '8.5.0' }).includes('worker_threads'))
+  assert(!builtins({ version: '10.5.0' }).includes('worker_threads'))
+  assert(
     builtins({ version: '10.5.0', experimental: true }).includes(
       'worker_threads'
     )
   )
-  t.ok(builtins({ version: '12.0.0' }).includes('worker_threads'))
-  t.end()
+  assert(builtins({ version: '12.0.0' }).includes('worker_threads'))
 })
 
 test('wasi', t => {
-  t.notOk(builtins({ version: '12.16.0' }).includes('wasi'))
-  t.ok(builtins({ version: '12.16.0', experimental: true }).includes('wasi'))
-  t.end()
+  assert(!builtins({ version: '12.16.0' }).includes('wasi'))
+  assert(builtins({ version: '12.16.0', experimental: true }).includes('wasi'))
 })
 
 test('diagnostics_channel', t => {
-  t.notOk(
-    builtins({ version: '15.0.0', experimental: true }).includes(
+  assert(
+    !builtins({ version: '15.0.0', experimental: true }).includes(
       'diagnostics_channel'
     )
   )
-  t.notOk(builtins({ version: '15.1.0' }).includes('diagnostics_channel'))
-  t.ok(
+  assert(!builtins({ version: '15.1.0' }).includes('diagnostics_channel'))
+  assert(
     builtins({ version: '15.1.0', experimental: true }).includes(
       'diagnostics_channel'
     )
   )
-  t.ok(
+  assert(
     builtins({ version: '15.2.0', experimental: true }).includes(
       'diagnostics_channel'
     )
   )
 
-  t.notOk(builtins({ version: '14.17.0' }).includes('diagnostics_channel'))
-  t.notOk(
-    builtins({ version: '14.16.0', experimental: true }).includes(
+  assert(!builtins({ version: '14.17.0' }).includes('diagnostics_channel'))
+  assert(
+    !builtins({ version: '14.16.0', experimental: true }).includes(
       'diagnostics_channel'
     )
   )
-  t.ok(
+  assert(
     builtins({ version: '14.17.0', experimental: true }).includes(
       'diagnostics_channel'
     )
   )
-  t.ok(
+  assert(
     builtins({ version: '14.18.0', experimental: true }).includes(
       'diagnostics_channel'
     )
   )
-
-  t.end()
 })
 
 test('default to current version', t => {
   for (const name of builtins()) {
-    t.ok(require(name), name)
+    assert(require(name), name)
   }
-  t.end()
 })
 
 test('returns all builtins with version *', t => {
-  t.equal(builtins({ version: '*' }).length, 32 + 9)
-  t.equal(builtins({ version: '*', experimental: true }).length, 32 + 9 + 2)
-  t.end()
+  assert.strictEqual(builtins({ version: '*' }).length, 32 + 9)
+  assert.strictEqual(
+    builtins({ version: '*', experimental: true }).length,
+    32 + 9 + 2
+  )
 })
